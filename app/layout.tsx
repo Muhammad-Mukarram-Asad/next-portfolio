b5bc3a08236f186c955f99452498faf48a14fee3
@muqarram.asad <mukarram.asad@hysabkytab.com>
2024-04-06 01:16:48 +0500

    Resolve Build Issue in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import StarsCanvas from "../components/main/Starbackground";
+import StarsCanvas from "../components/main/StarBackground";
 import Navbar from "../components/main/Navbar";
 import Footer from "../components/main/Footer";
 
